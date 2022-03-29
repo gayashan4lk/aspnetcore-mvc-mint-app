f6aad3b88182fc87e7267d05c1185040a9506b17
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-29 23:27:45 +0530

    build create item page

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mint.Models
 {
@@ -8,6 +9,7 @@ namespace Mint.Models
         public int Id { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
+        [DisplayName("Item name")]
         public string ItemName { get; set; }
     }
 }
