2ff222c985c213fe211dab39bd00fdce53ea3baf
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-29 20:12:51 +0530

    passing data to controller

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mint.Data;
+using Mint.Models;
+using System.Collections.Generic;
 
 namespace Mint.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly MintDbContext _db;
+
+        public ItemController(MintDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> ObjList = _db.Items;
+            return View(ObjList);
         }
     }
 }
