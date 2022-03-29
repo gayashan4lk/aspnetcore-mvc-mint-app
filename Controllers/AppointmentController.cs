6b8bc39ff73cf9a314e80d8126521bac8fb9e25d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-29 08:06:42 +0530

    add controller

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Mint.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+    }
+}
