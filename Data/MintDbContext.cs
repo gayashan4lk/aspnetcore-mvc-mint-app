47af78134c15ca8818f39d244d8d1b533333d6d9
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-29 10:44:54 +0530

    setup dbcontext

---
+++ Data/MintDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mint.Data
+{
+    public class MintDbContext: DbContext
+    {
+        public MintDbContext(DbContextOptions<MintDbContext> options): base(options)
+        {
+
+        }
+    }
+}
