f0cfb72b3d0df88c69fbf9dfe1fc6c2ad57e09c4
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-30 11:19:25 +0530

    add - delete expense

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -14,17 +14,20 @@ namespace Mint.Controllers
             _db = db;
         }
 
+        // Index
         public IActionResult Index()
         {
             IEnumerable<Expense> expensesList = _db.Expenses;
             return View(expensesList);
         }
 
+        // GET Create()
         public IActionResult Create()
         {
             return View();
         }
 
+        // POST Create()
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Expense expense)
@@ -38,5 +41,34 @@ namespace Mint.Controllers
             return View(expense);
         }
 
+        // POST Delete()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET Delete()
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
