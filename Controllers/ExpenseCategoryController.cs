daf95e63e482a1741685a06eac9b3aa2234bcf92
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-30 14:00:49 +0530

    add expense category model

---
+++ Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mint.Controllers
+{
+    public class ExpenseCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
